8d71b8952e8e015f955831797ca0fbc69dddf290
HirushiniChandrasoma <HirushiniRashmika.C@gmail.com>
2024-10-14 22:09:17 +0530

    Revert "delete dbConfig"
    This reverts commit c219dc9ecea5397967afee263fef5d06b4932359.

---
+++ server/config/dbConfig.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.MONGO_URL,{
+    dbName: 'csse_new_db',
+    
+})
+
+const connection = mongoose.connection;
+
+connection.on("connected", () => {
+    console.log("MongoDB is connected");
+});
+
+connection.on("error", () => {
+    console.log("Error in MongoDB connection",error);
+});
+
+module.exports = mongoose;
