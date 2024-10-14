652de602a2e8b7a8e335e8548da45e0d3812f386
Balasooriya B.M.P.U <piumiudeshika02@gmail.com>
2024-10-14 19:57:37 +0530

    update

--- server/models/Record.js
+++ server/models/Record.js
@@ -22,13 +22,7 @@ const recordSchema = new mongoose.Schema(
   }
 );
 
-recordSchema.connect((err) => {
-  if (err) {
-      console.error('Error connecting to the database:', err.stack);
-      return;
-  }
-  console.log('Connected to the database');
-});
+ 
 
 module.exports = mongoose.model("Record", recordSchema);
  
